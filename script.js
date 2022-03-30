1597c5f6695aeef8ef630c803e7ad4c7286150c2
Sanjith <85774398+Sanjith7146@users.noreply.github.com>
2022-03-30 12:21:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function createNoteElement(id, content) {
 
   element.classList.add("note");
   element.value = content;
-  element.placeholder = "Empty Sticky Note";
+  element.placeholder = "Empty Sticky Note, Double Click to delete";
 
   element.addEventListener("change", () => {
     updateNote(id, element.value);
